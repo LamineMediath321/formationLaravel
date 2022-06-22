ae0e9bcd9fa32b7a38ead7c3bab6b3482e1b9676
LamineMediath321 <89913597+LamineMediath321@users.noreply.github.com>
2022-06-22 23:11:22 +0200

    OneToMany

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -8,4 +8,13 @@
              <a class="btn btn-light" href="">Editer</a>
              <a class="btn btn-danger" href="{{route('delete_post',['id' => $post->id])}} ">Supprimer</a>
        </div>
+       <hr>
+       @forelse ($post->comments as $comment)
+           <div>
+                  <p>#{{$comment->id}} </p>
+                  {{$comment->content}} | crée en {{$comment->created_at->format('Y')}}
+           </div>
+       @empty
+           <p>Aucun commentaire </p>
+       @endforelse
 @endsection
--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Liste des articles</h1>
+   <div class="col-6 mx-auto">
+     <h1>Liste des articles</h1>
     @if ($posts)
         @foreach ($posts as $post)
              <h4><a href="{{route('app_post',['id' => $post->id])}} "><span>title: </span> {{$post->title}} </a></h4>
@@ -9,5 +10,6 @@
         @else
             <p class="text-center alert alert-danger">Aucun posts en base de donnees</p>
     @endif
+   </div>
     
 @endsection
